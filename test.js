1da5dedf9e431543532e44a15c547c2ccf13f642
Jagpat Singh <jsingh14192@gmail.com>
2021-05-11 23:39:07 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+let man = 1;
+function toggle() {
+    if(man == 1){
+        console.log('the value of man is 1');
+    }
+    else{
+        console.log('negative man');
+    }
+    man = -man;
+}
+toggle();
+toggle();
+toggle();
